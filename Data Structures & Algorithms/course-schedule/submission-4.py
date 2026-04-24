7e97e2299b55c9b4bbeba889c8b653bc37cb1f69
Andre Nguyen <andrenguyen073@gmail.com>
2026-04-24 13:34:19 -0400

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.py
@@ -0,0 +1,48 @@
+class Solution:
+    from collections import defaultdict
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adjacancy_list = defaultdict(list)
+        """ 
+        0: [1.2.3]
+        1: []
+        2: []
+        3: []
+        4: [5]
+        5: []
+        """
+        #create directed acyclic graphs 
+        for course, prerequisite in prerequisites: 
+            adjacancy_list[course].append(prerequisite)
+        
+        def search_cycles(course, seen):
+            if course in seen: 
+                return True
+            if course not in adjacancy_list: 
+                return False
+            seen.add(course)
+            prerequisites = adjacancy_list[course]
+            for prereq in prerequisites: 
+                return search_cycles(prereq, seen)
+                         
+        seen = set() 
+        for course in range(numCourses): 
+            #Cycle, a course can not be a prerequsite to each other
+            
+            if search_cycles(course, seen): 
+                return False
+        
+        return True
+        
+        
+
+        
+
+
+
+
+
+            
+        
+
+
+
