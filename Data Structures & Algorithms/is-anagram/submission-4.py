48f8e65ac0a2b944c6092259a609f374c2be8d70
Andre Nguyen <andrenguyen073@gmail.com>
2026-07-27 14:08:41 -0500

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+    from collections import defaultdict
+    def isAnagram(self, s: str, t: str) -> bool:
+        """ 
+        First thoughts get frequency maps for each if they match then its the same?
+        """
+
+        frequency_map = defaultdict(int)
+        for char in s: 
+            frequency_map[char] += 1 
+        print(frequency_map)
+
+        for char in t: 
+            if char in frequency_map:
+                frequency_map[char] -= 1 
+                if frequency_map[char] < 0: 
+                    return False
+            else: 
+                return False
+
+        return True
+            
