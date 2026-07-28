dee21dc72b2f823f3831ae80ce1151289b25db6f
Andre Nguyen <andrenguyen073@gmail.com>
2026-07-28 10:06:52 -0500

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    from collections import Counter
+    import heapq
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        """ 
+        could create a frequency map iterate over it find out the most frequent element 
+        - complexity could become larger if there is k = bigger number 
+        maybe sort num then iterate over it? 
+        """
+        if not nums: 
+            return []
+        frequency_map = Counter(nums)
+        max_heap= [(-value,frequency) for value, frequency in list(frequency_map.items())]
+        heapq.heapify(max_heap)
+        res = []
+        for i in range(k): 
+            val = heapq.heappop(max_heap)
+            res.append(val[1])
+        return res
+
+        
+
+
+
+
