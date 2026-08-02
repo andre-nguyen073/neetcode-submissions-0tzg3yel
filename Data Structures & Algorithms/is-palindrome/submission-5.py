940d4ebff6fa75b44c20a3ebfe2ff3d215c5f5b7
Andre Nguyen <andrenguyen073@gmail.com>
2026-08-01 22:03:39 -0500

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        """ 
+        Theres two cases odd case and even case: 
+        even ptr 1 
+
+        """
+            
+        s = "".join(char for char in s if char.isalnum())
+        if not s: 
+            return True
+        ptr1 = 0 
+        ptr2 = len(s) - 1
+        print(s)
+        while ptr1 != ptr2 or ptr2 < ptr1: 
+            if s[ptr1].lower() != s[ptr2].lower():
+                return False 
+            ptr1 += 1
+            ptr2 -= 1
+
+        return True
