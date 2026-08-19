98fe82e1a3a2f2da9807af4bfdb5f059b652c363
Andre Nguyen <andrenguyen073@gmail.com>
2026-08-19 11:32:15 -0500

    Add: remove-node-from-end-of-linked-list - submission-7

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-7.py
@@ -0,0 +1,51 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        """ 
+        removing the nth node from the end what does that involve - end node - n = node to remove right 
+        what if we just iterate over it and keep track of every node in a hashmap and then go to that exact node when we are done 
+        """ 
+        if not head: 
+            return None
+
+        #what if we lag a pointer n away 
+        ptr1 = head 
+        cnt = 0
+        ptr2 = head 
+
+        while ptr2.next:
+            if cnt != n: 
+                cnt += 1 
+            else: 
+                ptr1 = ptr1.next
+
+            ptr2 = ptr2.next 
+            
+        if ptr1 == head:
+            return head.next
+        #what happens if theres is nothing in front that would only happen in 0 case right
+        if not ptr1.next: 
+            ptr1.next = None
+        else:
+            ptr1.next = ptr1.next.next
+        #also there is a case where it is the first node
+
+        return head
+
+            
+        
+
+        
+        
+
+
+
+
+
+
+
